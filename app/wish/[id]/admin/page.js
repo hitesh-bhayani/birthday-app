5f4825ee2ccba8fe29038b82b18d0f2cbb396e5f
Hitesh Bhayani <hitesh.bhayani28@gmail.com>
2026-05-24 19:02:31 +0530

    cleanup: remove redundant admin redirect folder

--- app/wish/[id]/admin/page.js
+++
@@ -1,8 +0,0 @@
-// app/wish/[id]/admin/page.js
-import { redirect } from "next/navigation";
-
-export default async function WishAdminRedirect({ params }) {
-  const resolvedParams = await params;
-  const id = resolvedParams.id;
-  redirect(`/wish/${id}/edit`);
-}
